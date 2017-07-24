ffc5b280691b4266f02b228a9f1a70202ff4dd14
adrienne <adriennebing@gmail.com>
2017-07-24 15:11:24 -0400

    first commit

---
+++ src/HandToHand.java
@@ -0,0 +1,7 @@
+/**
+ * Created by adrienne on 7/24/17.
+ */
+public interface HandToHand {
+    void chokehold();
+    void twistarm();
+}
---
+++ src/MartialArts.java
@@ -0,0 +1,7 @@
+/**
+ * Created by adrienne on 7/24/17.
+ */
+public interface MartialArts {
+   void judo();
+   void aikido();
+}
